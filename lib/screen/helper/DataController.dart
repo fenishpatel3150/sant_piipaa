721d90bcbbacf75a857c2c750ac2d4ab3f18cf89
Fenish Patel <patelfenish34@gmail.com>
2024-06-24 13:00:15 +0530

    krishna krishna

--- lib/screen/helper/DataController.dart
+++ lib/screen/helper/DataController.dart
@@ -1,53 +1,32 @@
-
-
 import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:sant_piipaa/screen/model/datamodel.dart';
 import 'package:translator/translator.dart';
 
-
-
 class ApiService extends ChangeNotifier {
+  List<DataModel> Data = [];
+  List<String> translatedTexts = [];
 
-  List <DataModel> Data =[];
-  ApiService()
-  {
+  ApiService() {
     AllData();
   }
-  Future<void> AllData()
-  async {
-    String apiData =await rootBundle.loadString('assets/Json/data.json');
+
+  Future<void> AllData() async {
+    String apiData = await rootBundle.loadString('assets/Json/data.json');
     List json = jsonDecode(apiData);
 
-
-    Data =json.map((e) => DataModel.fromJson(e)).toList();
+    Data = json.map((e) => DataModel.fromJson(e)).toList();
+    translatedTexts = List<String>.filled(Data.length, '', growable: false);
 
     notifyListeners();
-
   }
-
 
   final translator = GoogleTranslator();
-  String _translatedText = '';
 
-  String get translatedText => _translatedText;
-
-  Future<void> translateText(String text, String LanguageCode) async {
-    Translation translation = await translator.translate(text, to: LanguageCode);
-    _translatedText = translation.text;
+  Future<void> translateText(String text, String languageCode, int index) async {
+    Translation translation = await translator.translate(text, to: languageCode);
+    translatedTexts[index] = translation.text;
     notifyListeners();
   }
-
 }
-
-
-
-
-
-
-
-
-
